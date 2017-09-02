48c1f8b1f48f3c47de6b3f160c5622913dc620e3
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:35:54 +0100

    Initial import

---
+++ Hack.h
@@ -0,0 +1,29 @@
+#ifndef _HACK_H
+#define _HACK_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Hack10[];
+    extern const uint8_t Hack11[];
+    extern const uint8_t Hack12[];
+    extern const uint8_t Hack13[];
+    extern const uint8_t Hack14[];
+    extern const uint8_t Hack15[];
+    extern const uint8_t Hack16[];
+    extern const uint8_t Hack17[];
+    extern const uint8_t Hack18[];
+    extern const uint8_t Hack19[];
+    extern const uint8_t Hack20[];
+    extern const uint8_t Hack21[];
+    extern const uint8_t Hack22[];
+    extern const uint8_t Hack23[];
+    extern const uint8_t Hack24[];
+    extern const uint8_t Hack25[];
+    extern const uint8_t Hack26[];
+    extern const uint8_t Hack27[];
+    extern const uint8_t Hack28[];
+    extern const uint8_t Hack29[];
+    extern const uint8_t Hack30[];
+};
+#endif
